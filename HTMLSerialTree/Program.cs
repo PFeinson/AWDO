deaa5ee6df2a85c81ffbde48a6cc273664fdbb03
Patrick Feinson <pfeinson@gmail.com>
2017-07-15 16:23:48 -0700

    Added HTMLSerialTree folder with working HTML construction capabilities

---
+++ HTMLSerialTree/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HTMLSerialTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HTMLTree test = new HTMLTree();
+            Console.WriteLine(test.toString());
+            //test.toString();
+        }
+    }
+}
